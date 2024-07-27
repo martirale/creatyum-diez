2cc570114ab14394dcf565e7bc484ef4e8d0f3c4
Alejandro Martir <hola@alemartir.com>
2024-07-27 09:47:35 +0000

    Add interest links and update Copyright

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,13 +4,39 @@ export default function Footer() {
   return (
     <footer className="bg-yellow text-black dark:bg-black dark:text-yellow p-6 mt-8">
       <div className="container mx-auto flex flex-col md:flex-row justify-between items-center">
-        <div className="flex items-center mb-8 md:mb-0">
+        <div className="flex items-center mb-16 md:mb-0">
           <Link href="/">
             <div className="logo-icon"></div>
           </Link>
         </div>
-        <div className="text-sm md:flex md:items-center">
-          <p>&copy; 2024 Creatyum Media</p>
+        <div className="flex flex-col text-center md:flex-row md:items-center space-y-5 md:space-y-0 md:space-x-8 mb-4 md:mb-0">
+          <Link href="/" className="text-lg hover:underline md:text-base">
+            Artículos
+          </Link>
+          <Link
+            href="/podcast"
+            className="text-lg hover:underline md:text-base"
+          >
+            Podcast
+          </Link>
+          <Link href="/about" className="text-lg hover:underline md:text-base">
+            About
+          </Link>
+          <Link
+            href="/collaborations"
+            className="text-lg hover:underline md:text-base"
+          >
+            Colaboraciones
+          </Link>
+          <Link
+            href="/contact"
+            className="text-lg hover:underline md:text-base"
+          >
+            Contacto
+          </Link>
+        </div>
+        <div className="text-sm p-12 text-center md:flex md:items-center md:text-base md:p-0">
+          <p>Creatyum &copy; 2024 una marca de Alejandro Mártir</p>
         </div>
       </div>
     </footer>
