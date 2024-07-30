046f0eb27f42a3c8b1662ec3f6f4a7e959c914bf
Alejandro Martir <hola@alemartir.com>
2024-07-30 06:55:46 +0000

    Remove innecesary content

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -5,20 +5,21 @@ export default function Timeline() {
         <div className="grid gap-4 mx-4 sm:grid-cols-12">
           <div className="col-span-12 sm:col-span-3">
             <div className="text-center sm:text-left mb-14 before:block before:w-24 before:h-3 before:mb-5 before:rounded-md before:mx-auto sm:before:mx-0">
-              <h3 className="text-3xl font-semibold">Morbi tempor</h3>
+              <h3 className="text-3xl font-semibold">Creatyum</h3>
               <span className="text-sm font-bold tracking-wider uppercase">
-                Vestibulum diam nunc
+                en el tiempo...
               </span>
             </div>
           </div>
           <div className="relative col-span-12 px-4 space-y-6 sm:col-span-9">
             <div className="col-span-12 space-y-12 relative px-4 sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:w-0.5 sm:before:-left-3 before:bg-black before:dark:bg-yellow">
+              {/* Event */}
               <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-black before:dark:bg-yellow">
                 <h3 className="text-xl font-semibold tracking-wide">
-                  Donec porta enim vel{" "}
+                  Lorem ipsum dolor sit amet
                 </h3>
                 <time className="text-xs tracking-wide uppercase">
-                  Dec 2020
+                  MMM YYYY
                 </time>
                 <p className="mt-3">
                   Pellentesque feugiat ante at nisl efficitur, in mollis orci
@@ -26,33 +27,7 @@ export default function Timeline() {
                   in faucibus.
                 </p>
               </div>
-              <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-black before:dark:bg-yellow">
-                <h3 className="text-xl font-semibold tracking-wide">
-                  Aliquam sit amet nunc ut
-                </h3>
-                <time className="text-xs tracking-wide uppercase">
-                  Jul 2019
-                </time>
-                <p className="mt-3">
-                  Morbi vulputate aliquam libero non dictum. Aliquam sit amet
-                  nunc ut diam aliquet tincidunt nec nec dui. Donec mollis
-                  turpis eget egestas sodales.
-                </p>
-              </div>
-              <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-black before:dark:bg-yellow">
-                <h3 className="text-xl font-semibold tracking-wide">
-                  Pellentesque habitant morbi
-                </h3>
-                <time className="text-xs tracking-wide uppercase">
-                  Jan 2016
-                </time>
-                <p className="mt-3">
-                  Suspendisse tincidunt, arcu nec faucibus efficitur, justo
-                  velit consectetur nisl, sit amet condimentum lacus orci nec
-                  purus. Mauris quis quam suscipit, vehicula felis id, vehicula
-                  enim.
-                </p>
-              </div>
+              {/* Event End */}
             </div>
           </div>
         </div>
