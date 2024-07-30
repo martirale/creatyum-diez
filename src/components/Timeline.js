b0208c4e2a089590804529d5082ea434f24ebf6d
Alejandro Martir <hola@alemartir.com>
2024-07-30 08:04:15 +0000

    Integrate Strapi API

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,4 +1,51 @@
+"use client";
+import { useState, useEffect } from "react";
+
 export default function Timeline() {
+  const [events, setEvents] = useState([]);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    async function fetchEvents() {
+      try {
+        const res = await fetch("http://localhost:1337/api/timeline-events");
+        if (res.ok) {
+          const data = await res.json();
+          const formattedEvents = data.data.map((event) => ({
+            id: event.id,
+            title: event.attributes.title,
+            date: event.attributes.date,
+            description: event.attributes.description
+              .map((desc) => desc.children.map((child) => child.text).join(" "))
+              .join("\n"),
+          }));
+          setEvents(formattedEvents);
+        } else {
+          setError("Failed to fetch events");
+        }
+      } catch (error) {
+        setError("Error fetching events");
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchEvents();
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (error) {
+    return <p>{error}</p>;
+  }
+
+  if (!events || events.length === 0) {
+    return <p>No hay eventos para mostrar.</p>;
+  }
+
   return (
     <section>
       <div className="container max-w-5xl px-4 py-12 mx-auto">
@@ -13,21 +60,23 @@ export default function Timeline() {
           </div>
           <div className="relative col-span-12 px-4 space-y-6 sm:col-span-9">
             <div className="col-span-12 space-y-12 relative px-4 sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:w-0.5 sm:before:-left-3 before:bg-black before:dark:bg-yellow">
-              {/* Event */}
-              <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-black before:dark:bg-yellow">
-                <h3 className="text-xl font-semibold tracking-wide">
-                  Lorem ipsum dolor sit amet
-                </h3>
-                <time className="text-xs tracking-wide uppercase">
-                  MMM YYYY
-                </time>
-                <p className="mt-3">
-                  Pellentesque feugiat ante at nisl efficitur, in mollis orci
-                  scelerisque. Interdum et malesuada fames ac ante ipsum primis
-                  in faucibus.
-                </p>
-              </div>
-              {/* Event End */}
+              {events.map((event) => (
+                <div
+                  key={event.id}
+                  className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-black before:dark:bg-yellow"
+                >
+                  <h3 className="text-xl font-semibold tracking-wide">
+                    {event.title}
+                  </h3>
+                  <time className="text-xs tracking-wide uppercase">
+                    {new Date(event.date).toLocaleDateString("es-ES", {
+                      year: "numeric",
+                      month: "short",
+                    })}
+                  </time>
+                  <p className="mt-3">{event.description}</p>
+                </div>
+              ))}
             </div>
           </div>
         </div>
