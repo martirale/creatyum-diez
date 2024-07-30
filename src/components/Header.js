59dddc657639b96767510ea720ff0b55c8e7e100
Alejandro Martir <hola@alemartir.com>
2024-07-30 07:28:56 +0000

    Fixed header z-index

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,7 @@ import Navbar from "../components/Navbar.js";
 
 export default function Header() {
   return (
-    <header className="fixed w-full bg-yellow text-black dark:bg-black dark:text-yellow p-6">
+    <header className="fixed w-full p-6 bg-yellow text-black dark:bg-black dark:text-yellow z-50">
       <Navbar />
     </header>
   );
