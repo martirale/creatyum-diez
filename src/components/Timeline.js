310fdb2e356078460d9f60b22b4dfd14743313b4
Alejandro Martir <hola@alemartir.com>
2024-07-30 06:39:55 +0000

    Apply brand colors

--- src/components/Timeline.js
+++ src/components/Timeline.js
@@ -1,23 +1,23 @@
 export default function Timeline() {
   return (
-    <section className="dark:bg-gray-100 dark:text-gray-800">
+    <section>
       <div className="container max-w-5xl px-4 py-12 mx-auto">
         <div className="grid gap-4 mx-4 sm:grid-cols-12">
           <div className="col-span-12 sm:col-span-3">
-            <div className="text-center sm:text-left mb-14 before:block before:w-24 before:h-3 before:mb-5 before:rounded-md before:mx-auto sm:before:mx-0 before:dark:bg-violet-600">
+            <div className="text-center sm:text-left mb-14 before:block before:w-24 before:h-3 before:mb-5 before:rounded-md before:mx-auto sm:before:mx-0">
               <h3 className="text-3xl font-semibold">Morbi tempor</h3>
-              <span className="text-sm font-bold tracking-wider uppercase dark:text-gray-600">
+              <span className="text-sm font-bold tracking-wider uppercase">
                 Vestibulum diam nunc
               </span>
             </div>
           </div>
           <div className="relative col-span-12 px-4 space-y-6 sm:col-span-9">
-            <div className="col-span-12 space-y-12 relative px-4 sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:w-0.5 sm:before:-left-3 before:dark:bg-gray-300">
-              <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:dark:bg-violet-600">
+            <div className="col-span-12 space-y-12 relative px-4 sm:col-span-8 sm:space-y-8 sm:before:absolute sm:before:top-2 sm:before:bottom-0 sm:before:w-0.5 sm:before:-left-3 before:bg-black before:dark:bg-yellow">
+              <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-black before:dark:bg-yellow">
                 <h3 className="text-xl font-semibold tracking-wide">
                   Donec porta enim vel{" "}
                 </h3>
-                <time className="text-xs tracking-wide uppercase dark:text-gray-600">
+                <time className="text-xs tracking-wide uppercase">
                   Dec 2020
                 </time>
                 <p className="mt-3">
@@ -26,11 +26,11 @@ export default function Timeline() {
                   in faucibus.
                 </p>
               </div>
-              <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:dark:bg-violet-600">
+              <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-black before:dark:bg-yellow">
                 <h3 className="text-xl font-semibold tracking-wide">
                   Aliquam sit amet nunc ut
                 </h3>
-                <time className="text-xs tracking-wide uppercase dark:text-gray-600">
+                <time className="text-xs tracking-wide uppercase">
                   Jul 2019
                 </time>
                 <p className="mt-3">
@@ -39,11 +39,11 @@ export default function Timeline() {
                   turpis eget egestas sodales.
                 </p>
               </div>
-              <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:dark:bg-violet-600">
+              <div className="flex flex-col sm:relative sm:before:absolute sm:before:top-2 sm:before:w-4 sm:before:h-4 sm:before:rounded-full sm:before:left-[-35px] sm:before:z-[1] before:bg-black before:dark:bg-yellow">
                 <h3 className="text-xl font-semibold tracking-wide">
                   Pellentesque habitant morbi
                 </h3>
-                <time className="text-xs tracking-wide uppercase dark:text-gray-600">
+                <time className="text-xs tracking-wide uppercase">
                   Jan 2016
                 </time>
                 <p className="mt-3">
