56c6e1c4b28d29f065e6a581b1bb13d7c7623521
Alejandro Martir <hola@alemartir.com>
2024-07-30 21:27:29 +0000

    Add episodes filter and update cover episode design

--- src/components/PodcastPlayer.js
+++ src/components/PodcastPlayer.js
@@ -1,41 +1,45 @@
 "use client";
 
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Parser from "rss-parser";
-
-const parser = new Parser();
 
 const PodcastPlayer = () => {
   const [episodes, setEpisodes] = useState([]);
+  const keyword = "temporada";
+  const rssUrl = "https://anchor.fm/s/a59b2a8/podcast/rss";
 
   useEffect(() => {
-    const fetchPodcast = async () => {
+    const fetchEpisodes = async () => {
+      const parser = new Parser();
       try {
-        const feed = await parser.parseURL(
-          "https://anchor.fm/s/a59b2a8/podcast/rss"
+        const feed = await parser.parseURL(rssUrl);
+        const filteredEpisodes = feed.items.filter(
+          (item) =>
+            item.content.includes(keyword) ||
+            item.contentSnippet.includes(keyword)
         );
-        setEpisodes(feed.items);
+        setEpisodes(filteredEpisodes);
       } catch (error) {
-        console.error("Error fetching podcast feed:", error);
+        console.error("Error fetching podcast episodes:", error);
       }
     };
 
-    fetchPodcast();
+    fetchEpisodes();
   }, []);
 
   return (
-    <div className="container mx-auto p-4">
-      <h2 className="text-2xl font-bold mb-4">Podcast</h2>
-      <div className="space-y-4">
+    <section className="container mx-auto p-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {episodes.map((episode) => (
-          <div key={episode.guid} className="p-4 border rounded-lg shadow">
-            <img
-              src={episode.itunes.image}
-              alt={episode.title}
-              className="w-full h-48 object-cover mb-4 rounded-lg"
-            />
+          <div key={episode.guid} className="bg-white shadow-md rounded-lg p-4">
+            <div className="w-100 h-100 mb-4 overflow-hidden rounded-lg">
+              <img
+                src={episode.itunes.image}
+                alt={episode.title}
+                className="w-full h-full object-cover"
+              />
+            </div>
             <h3 className="text-xl font-semibold">{episode.title}</h3>
-            <p className="text-gray-700">{episode.contentSnippet}</p>
             <audio controls className="w-full mt-4">
               <source src={episode.enclosure.url} type="audio/mpeg" />
               Your browser does not support the audio element.
@@ -43,7 +47,7 @@ const PodcastPlayer = () => {
           </div>
         ))}
       </div>
-    </div>
+    </section>
   );
 };
 
