263c49cd0210fbb95b9239a071a4330ae3cfdb9a
Alejandro Martir <hola@alemartir.com>
2024-07-31 08:22:36 +0000

    Create timeline page

---
+++ src/app/timeline/page.js
@@ -0,0 +1,16 @@
+import Timeline from "@/components/Timeline.js";
+
+export const metadata = {
+  title: "Timeline — Creatyum Media",
+  description: "Revista digital para diseñadores y creativos en Latam.",
+};
+
+export default function Collabs() {
+  return (
+    <div>
+      <h1>Timeline</h1>
+
+      <Timeline />
+    </div>
+  );
+}
