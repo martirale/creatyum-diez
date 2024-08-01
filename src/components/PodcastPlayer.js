7971a39e4dbfe72a24715629dcd2f35de613b3f1
Alejandro Martir <hola@alemartir.com>
2024-08-01 21:56:03 +0000

    Set featured episode

--- src/components/PodcastPlayer.js
+++ src/components/PodcastPlayer.js
@@ -4,7 +4,8 @@ import React, { useState, useEffect } from "react";
 import Parser from "rss-parser";
 
 const PodcastPlayer = () => {
-  const [episodes, setEpisodes] = useState([]);
+  const [latestEpisode, setLatestEpisode] = useState(null);
+  const [otherEpisodes, setOtherEpisodes] = useState([]);
   const keyword = "temporada";
   const rssUrl = "https://anchor.fm/s/a59b2a8/podcast/rss";
 
@@ -18,7 +19,11 @@ const PodcastPlayer = () => {
             item.content.includes(keyword) ||
             item.contentSnippet.includes(keyword)
         );
-        setEpisodes(filteredEpisodes);
+
+        if (filteredEpisodes.length > 0) {
+          setLatestEpisode(filteredEpisodes[0]);
+          setOtherEpisodes(filteredEpisodes.slice(1));
+        }
       } catch (error) {
         console.error("Error fetching podcast episodes:", error);
       }
@@ -29,8 +34,29 @@ const PodcastPlayer = () => {
 
   return (
     <section className="container mx-auto px-8 py-8 md:px-0">
+      {latestEpisode && (
+        <div className="mb-8">
+          <div className="bg-yellow text-black border border-solid border-black hover:bg-black hover:text-yellow dark:bg-black dark:text-yellow dark:hover:bg-yellow dark:hover:text-black dark:border-yellow">
+            <div className="w-100 h-100 overflow-hidden">
+              <img
+                src={latestEpisode.itunes.image}
+                alt={latestEpisode.title}
+                className="w-full h-full object-cover"
+              />
+            </div>
+            <div className="p-4">
+              <h3 className="text-lg">{latestEpisode.title}</h3>
+              <audio controls className="w-full mt-4">
+                <source src={latestEpisode.enclosure.url} type="audio/mpeg" />
+                Your browser does not support the audio element.
+              </audio>
+            </div>
+          </div>
+        </div>
+      )}
+
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
-        {episodes.map((episode) => (
+        {otherEpisodes.map((episode) => (
           <div
             key={episode.guid}
             className="bg-yellow text-black border border-solid border-black hover:bg-black hover:text-yellow dark:bg-black dark:text-yellow dark:hover:bg-yellow dark:hover:text-black dark:border-yellow"
