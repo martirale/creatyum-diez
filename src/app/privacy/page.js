265234ef279973cbfb399d89e2343f0255d4e025
Alejandro Martir <hola@alemartir.com>
2024-08-03 20:24:43 +0000

    Add init config

---
+++ src/app/privacy/page.js
@@ -0,0 +1,7 @@
+export default function Privacy() {
+  return (
+    <div>
+      <h1>Política de privacidad</h1>
+    </div>
+  );
+}
---
+++ src/app/terms/page.js
@@ -0,0 +1,7 @@
+export default function Terms() {
+  return (
+    <div>
+      <h1>Términos y condiciones</h1>
+    </div>
+  );
+}
