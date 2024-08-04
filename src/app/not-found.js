4e33f1c0286bd531d4968f85a578e7073caf2e0d
Alejandro Martir <hola@alemartir.com>
2024-08-04 03:06:09 +0000

    Create custom 404

---
+++ src/app/not-found.js
@@ -0,0 +1,28 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faHouse } from "@fortawesome/free-solid-svg-icons";
+import Link from "next/link";
+
+export default function NotFound() {
+  return (
+    <div className="container mx-auto px-8 py-8 md:px-0">
+      <h1>Sobre Creatyum</h1>
+
+      <div className="grid grid-cols-12 gap-4 md:gap-12">
+        <div className="col-span-12 md:col-span-9">
+          <h2 className="font-extrabold text-7xl pb-16 md:text-9xl">
+            404 Not Found
+          </h2>
+          <p className="text-2xl mb-12">
+            Lo sentimos, la página que buscas no existe.
+          </p>
+          <Link
+            href="/"
+            className="bg-yellow text-black px-8 py-4 border border-solid border-black hover:bg-black hover:text-yellow dark:bg-black dark:text-yellow dark:border-yellow dark:hover:bg-yellow dark:hover:text-black"
+          >
+            <FontAwesomeIcon icon={faHouse} /> Regresar al inicio
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
