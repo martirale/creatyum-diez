fdfe91de3b15b71c83b3fc026cca64242ebf36ef
Alejandro Martir <hola@alemartir.com>
2024-08-05 04:19:43 +0000

    Fixed heading

--- src/app/not-found.js
+++ src/app/not-found.js
@@ -5,7 +5,7 @@ import { faHouse } from "@fortawesome/free-solid-svg-icons";
 export default function NotFound() {
   return (
     <div className="container mx-auto px-8 py-8 md:px-0">
-      <h1>Sobre Creatyum</h1>
+      <h1>404 Not Found</h1>
 
       <div className="grid grid-cols-12 gap-4 md:gap-12">
         <div className="col-span-12 md:col-span-9">
